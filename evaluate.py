2a9bc66ddf60c0d95190595db8c98f0e67673677
Pedro Sandoval <psandovalsegura@gmail.com>
2024-10-29 01:22:52 -0400

    update evaluate scripts

--- evaluate.py
+++ evaluate.py
@@ -1,7 +1,9 @@
 import os
+import re
 import json
 import lm_eval
 import argparse
+from pathlib import Path
 from transformers import AutoModelForCausalLM, AutoTokenizer
 
 def main():
@@ -14,7 +16,7 @@ def main():
     parser.add_argument('--device', type=str, default='cuda')
     parser.add_argument('--peft', type=str, default='')
     parser.add_argument('--hf_cache_dir', type=str, default='/fs/nexus-scratch/psando/huggingface')
-    parser.add_argument('--output_dir', type=str, default='evaluate_results')
+    parser.add_argument('--save_samples_in_results', action='store_true', help='Save generated samples in the results file')
     args = parser.parse_args()
     print(args)
     using_peft = len(args.peft) > 0
@@ -30,19 +32,27 @@ def main():
         num_fewshot=args.num_fewshot,
         device=args.device,
     )
-    
-    # Save results in json format
-    os.makedirs(args.output_dir, exist_ok=True)
-    if using_peft:
-        results_file = f"results_{args.peft.split('/')[-2]}.json"
-    else:
-        results_file = f"results_{args.pretrained_model_name_or_path.split('/')[-2]}.json"
-    with open(os.path.join(args.output_dir, results_file), "w") as f:
-        json.dump(results, f, indent=4, default=lm_eval.utils.handle_non_serializable, ensure_ascii=False)
 
-    print(f"Results saved in {results_file}")
-    print(f"Strict match: {results['results']['gsm8k']['exact_match,strict-match'] * 100: 0.2f}%")
-    print(f"Flexible match: {results['results']['gsm8k']['exact_match,flexible-extract'] * 100: 0.2f}%")
+    # Delete the the model directory and replace it with the results file
+    if using_peft:
+        os.system(f"rm -r {args.peft}")
+        slurm_id_dir = Path(args.peft).parent
+        iter_num = re.search(r'ckpt_iter_(\d+)_model', args.peft).group(1)
+    else:
+        os.system(f"rm -r {args.pretrained_model_name_or_path}")
+        slurm_id_dir = Path(args.pretrained_model_name_or_path).parent
+        iter_num = re.search(r'ckpt_iter_(\d+)_model', args.pretrained_model_name_or_path).group(1)
+
+    # Replace the model directory with the results file
+    # which will be placed at {base_dir}/results.json
+    results_file = f"results_iter_{iter_num}.json"
+    with open(slurm_id_dir / results_file, "w") as f:
+        if args.save_samples_in_results:
+            json.dump(results, f, indent=4, default=lm_eval.utils.handle_non_serializable, ensure_ascii=False)
+        else: # remove the samples from results to save space
+            results.pop('samples')
+            json.dump(results, f, indent=4, default=lm_eval.utils.handle_non_serializable, ensure_ascii=False)
+
 
 if __name__ == "__main__":
     main()
--- scripts/finetuning/evaluate-ckpt.sh
+++ scripts/finetuning/evaluate-ckpt.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 #SBATCH --account=scavenger
 #SBATCH --job-name=eval-gsm8k
-#SBATCH --time=1:00:00
+#SBATCH --time=0:30:00
 #SBATCH --partition=scavenger
 #SBATCH --qos=scavenger
 #SBATCH --ntasks=1
 #SBATCH --gres=gpu:rtxa6000:1
-#SBATCH --cpus-per-task=4
+#SBATCH --cpus-per-task=1
 #SBATCH --mem=32G
 #SBATCH --output=/dev/null
-#--SBATCH --dependency=afterok:3246188
 
 export TOKENIZERS_PARALLELISM=true
 
